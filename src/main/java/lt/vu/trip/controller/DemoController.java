546606fd4831d4feca7b50aee91b9dc4309af630
Martynas Šapalas <martynas.sapalas@gmail.com>
2019-03-02 11:48:23 +0200

    Add checkstyle and editorconfig

--- src/main/java/lt/vu/trip/controller/DemoController.java
+++ src/main/java/lt/vu/trip/controller/DemoController.java
@@ -1,18 +1,16 @@
 package lt.vu.trip.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
-    @GetMapping("")
-    public String all() {
-        return "{\"success\":1}";
-    }
+	@GetMapping("")
+	public String all() {
+		return "{\"success\":1}";
+	}
 }
+
--- src/test/java/lt/vu/trip/TripApplicationTests.java
+++ src/test/java/lt/vu/trip/TripApplicationTests.java
@@ -2,7 +2,9 @@ package lt.vu.trip;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
