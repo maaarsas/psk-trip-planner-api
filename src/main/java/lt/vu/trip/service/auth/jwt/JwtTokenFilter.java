be0a08a7945de664106ad14bffe54fcc86b41fc2
kazimieras.povilas@gmail.com <kazimieras.povilas@gmail.com>
2019-04-02 15:33:37 +0300

    fixed authentication exception handling

--- src/main/java/lt/vu/trip/service/auth/jwt/JwtTokenFilter.java
+++ src/main/java/lt/vu/trip/service/auth/jwt/JwtTokenFilter.java
@@ -1,17 +1,20 @@
 package lt.vu.trip.service.auth.jwt;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.security.sasl.AuthenticationException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtTokenFilter extends GenericFilterBean {
+@Slf4j
+public class JwtTokenFilter extends OncePerRequestFilter {
 
 	private JwtTokenProvider jwtTokenProvider;
 
@@ -20,13 +23,30 @@ public class JwtTokenFilter extends GenericFilterBean {
 	}
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
-			throws IOException, ServletException {
-		String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
-		if (token != null && jwtTokenProvider.validateToken(token)) {
-			Authentication auth = jwtTokenProvider.getAuthentication(token);
-			SecurityContextHolder.getContext().setAuthentication(auth);
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+		try {
+			String token = jwtTokenProvider.resolveToken(request);
+			if (token != null && jwtTokenProvider.validateToken(token)) {
+				Authentication auth = jwtTokenProvider.getAuthentication(token);
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+			filterChain.doFilter(request, response);
+		} catch (InvalidJwtAuthenticationException e) {
+			log.debug("InvalidJwtAuthenticationException: " + e.getMessage());
+			sendError(response, "Invalid or expired token");
+		} catch (AuthenticationException e) {
+			log.debug("AuthenticationException: " + e.getMessage());
+			sendError(response, "Authentication failure");
+		} catch (RuntimeException e) {
+			log.debug("RuntimeException: " + e.getMessage());
+			sendError(response, "Error during authentication");
 		}
-		filterChain.doFilter(req, res);
+	}
+
+	private void sendError(HttpServletResponse response, String message) throws IOException {
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		response.getWriter().write(message);
+		response.getWriter().flush();
+		response.getWriter().close();
 	}
 }
