ccd83e5c2041281197d9aa2fcfbf98565882dcc7
Martynas Šapalas <martynas.sapalas@gmail.com>
2019-05-10 22:40:51 +0300

    Implement trip mergeing functionality

---
+++ src/main/java/lt/vu/trip/service/trip/TripMergeException.java
@@ -0,0 +1,8 @@
+package lt.vu.trip.service.trip;
+
+public class TripMergeException extends RuntimeException {
+
+	public TripMergeException(String message) {
+		super(message);
+	}
+}
--- src/main/java/lt/vu/trip/service/trip/TripMergeServiceImpl.java
+++ src/main/java/lt/vu/trip/service/trip/TripMergeServiceImpl.java
@@ -1,6 +1,9 @@
 package lt.vu.trip.service.trip;
 
 import lt.vu.trip.entity.Trip;
+import lt.vu.trip.entity.TripParticipation;
+import lt.vu.trip.entity.response.ErrorType;
+import lt.vu.trip.repository.TripParticipationRepository;
 import lt.vu.trip.repository.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
@@ -13,6 +16,9 @@ public class TripMergeServiceImpl implements TripMergeService {
 
 	@Autowired
 	private TripRepository tripRepository;
+
+	@Autowired
+	private TripParticipationRepository tripParticipationRepository;
 
 	public List<Trip> getTripsMergeableToTrip(Long toTripId) {
 		Trip toTrip = tripRepository.findById(toTripId).orElseThrow(ResourceNotFoundException::new);
@@ -31,6 +37,19 @@ public class TripMergeServiceImpl implements TripMergeService {
 	}
 
 	public Trip mergeTrips(Long toTripId, Long mergeableTripId) {
-		return null;
+		Trip toTrip = tripRepository.findById(toTripId).orElseThrow(ResourceNotFoundException::new);
+		Trip mergeableTrip = tripRepository.findById(mergeableTripId).orElseThrow(ResourceNotFoundException::new);
+		List<Trip> mergeableTrips = getTripsMergeableToTrip(toTrip.getId());
+
+		if (!mergeableTrips.stream().anyMatch(trip -> trip.getId() == mergeableTrip.getId())) {
+			throw new TripMergeException(ErrorType.TRIP_MERGE_NOT_MERGEABLE.toString());
+		}
+		// move all trip participations
+		for (TripParticipation tripParticipation : mergeableTrip.getTripParticipations()) {
+			tripParticipation.setTrip(toTrip);
+			tripParticipationRepository.save(tripParticipation);
+		}
+		tripRepository.delete(mergeableTrip);
+		return toTrip;
 	}
 }
